b33329eb4af49abff1b7953e499f92148be4291c
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-10-02 18:16:03 -0500

    Initial Commit

---
+++ Pipeline/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pipeline
+//
+//  Created by Charles Martin Reed on 10/2/18.
+//  Copyright © 2018 Charles Martin Reed. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pipeline/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pipeline
+//
+//  Created by Charles Martin Reed on 10/2/18.
+//  Copyright © 2018 Charles Martin Reed. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
